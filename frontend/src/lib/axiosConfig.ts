f704b7031ce55ecb714ae7317514cbfb3c25978c
최동원 <dwonc2@naver.com>
2026-02-18 15:25:03 +0900

    fix: axiosConfig baseURL fallback 수정

--- frontend/src/lib/axiosConfig.ts
+++ frontend/src/lib/axiosConfig.ts
@@ -1,9 +1,7 @@
 import axios from "axios";
 
-// axios 전역 설정
 axios.defaults.withCredentials = true;
 axios.defaults.baseURL =
-  import.meta.env.VITE_API_URL ??
-  "import.meta.env.VITE_API_URL || 'https://api.it-da.cloud'";
+  import.meta.env.VITE_API_URL || "https://api.it-da.cloud";
 
 export default axios;
